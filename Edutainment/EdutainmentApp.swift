5fb86dc2b5afc9166f03c45e26412a5ab81b1f7e
leotn03 <leo.tn03@icloud.com>
2024-01-04 21:10:41 -0500

    Initial Commit

---
+++ Edutainment/EdutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentApp.swift
+//  Edutainment
+//
+//  Created by Leo Torres Neyra on 4/1/24.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
